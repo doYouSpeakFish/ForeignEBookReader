1d4ed627c8fe4b289a6c4a217e80db7217a53a88
doYouSpeakFish <ed_sills@hotmail.co.uk>
2021-06-09 15:33:34 +0100

    wrote DB inserts and queries

--- app/src/main/java/com/example/foreignebookreader/AppDao.java
+++ app/src/main/java/com/example/foreignebookreader/AppDao.java
@@ -1,5 +1,31 @@
 package com.example.foreignebookreader;
 
-public class AppDao {
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.foreignebookreader.DbEntities.EntityBook;
+import com.example.foreignebookreader.DbEntities.EntityTranslation;
+
+import java.util.List;
+
+@Dao
+public interface AppDao {
+
+    @Insert
+    public void insertBook(EntityBook entityBook);
+
+    @Insert
+    public void insertTranslation(EntityTranslation entityTranslation);
+
+    @Query(value = "SELECT * FROM EntityBook")
+    public LiveData<List<EntityBook>> getBooks();
+
+    @Query(value = "SELECT * FROM EntityTranslation " +
+            "WHERE text = :text " +
+            "AND sourceLang = :sourceLang " +
+            "AND targetLang = :targetLang")
+    public LiveData<EntityTranslation> getTranslation(String text, String sourceLang, String targetLang);
 
 }
